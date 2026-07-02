be8e440347ae84c14ae13771e22c1191608e12ae
Steve Sparks <steve@senzall.dev>
2026-07-02 08:33:25 -0500

    pouncesql 0.1.8

--- Casks/pouncesql.rb
+++ Casks/pouncesql.rb
@@ -1,6 +1,6 @@
 cask "pouncesql" do
-  version "0.1.6"
-  sha256 "0165c8a6f7ec9bc2f9f3a95e1de9afed612b6f36a054baa2fe0b742246a2898a"
+  version "0.1.8"
+  sha256 "b32335f014a1b280235d640e002f20db32ef332a708735039404492756ba8466"
 
   url "https://github.com/senzalldev/pouncesql-app/releases/download/v#{version}/PounceSQL-#{version}.dmg"
   name "PounceSQL"
