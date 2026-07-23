ef2707f63f3534517aed28131e931a1032c5b3f0
senzalldev <stevesparks@wustl.edu>
2026-07-23 12:06:43 -0500

    senzalls-tower: update to 1.0.12

--- Casks/senzalls-tower.rb
+++ Casks/senzalls-tower.rb
@@ -1,7 +1,7 @@
 cask "senzalls-tower" do
-  version "1.0.11"
-  sha256 "055287e5d3ac4a423b17e55fccb138c98ccdd29c53f377c74df98ed8a807db2c"
-  url "https://github.com/senzalldev/senzalls-tower/releases/download/v1.0.11/Senzalls-Tower-1.0.11.dmg"
+  version "1.0.12"
+  sha256 "8ad7fce20d6cbecb38524d3b9b1383dea6aaf605af63858151f4ad5f26fee371"
+  url "https://github.com/senzalldev/senzalls-tower/releases/download/v1.0.12/Senzalls-Tower-1.0.12.dmg"
   name "Senzall's Tower"
   desc "Offline single-player tower-building simulation game"
   homepage "https://senzall.com"
